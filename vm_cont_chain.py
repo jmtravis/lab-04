15a916135f8cecac9c9a1e0696747d718cb57934
jmtravis <122946408+jmtravis@users.noreply.github.com>
2023-02-17 15:27:12 -0800

    Update vm_cont_chain.py

--- vm_cont_chain.py
+++ vm_cont_chain.py
@@ -57,7 +57,7 @@ if __name__ == '__main__':
     server in the event no messages have been published from or sent to this 
     client. If the connection request is successful, the callback attached to
     `client.on_connect` will be called."""    
-    client.connect(host="172.20.10.5", port=11000, keepalive=60)
+    client.connect(host="172.20.10.5", port=1883, keepalive=60)
 
     """In our prior labs, we did not use multiple threads per se. Instead, we
     wrote clients and servers all in separate *processes*. However, every 
